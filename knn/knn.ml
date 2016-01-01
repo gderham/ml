b90beeaa4804b0068a9fe9a474f4cb4efe16feaa
Guy Derham <guy.derham@gmail.com>
2016-01-01 16:30:09 +0000

    Tidy, and print useful messages while computing.

--- knn/knn.ml
+++ knn/knn.ml
@@ -26,7 +26,7 @@
 
 open Mnist
 open Core.Std
-
+open Printf
 
 let base_dir = "/Users/guy/repos/ml/mnist/"
 
@@ -92,60 +92,63 @@ let m_sum m = (* generate a vector of the row sums of a matrix *)
 
 let sos m = m |> m_sq |> m_sum;;
 
-
 let squared_euclidean_distance p q =
   add
     (bsx_add (sos p) (sos q))
     (scale (-2) (multiply p (transpose q)))
 ;;
 
-(* For an nxm matrix, find the minimum element of each row, and return a corresponding 2xm matrix of min,index of min *)
-(* The index starts at 0. *)
-let min_index m = (* TODO: rewrite with folding *)
-  let rec find_min min min_indx curr_indx = function
-    | [] -> (min,min_indx)
-    | h :: t -> if h < min
-          then find_min h   curr_indx (curr_indx+1) t
-          else find_min min min_indx  (curr_indx+1) t in
-  List.map m (fun row -> find_min 99999 (-1) 0 row)
-;;    
+(* For an nxm matrix, find the minimum element of each row, and return a
+   corresponding 2xm matrix of (index,val). The index starts at 0.
+*)
+let find_min_index_val list =
+  if list = [] then
+    failwith "Cannot find minimum value of empty list."
+  else
+    let find_min curr_idx idx_min x =
+      match idx_min with
+      | (-1,_) -> (curr_idx, x)
+      | (i,m) when x < m -> (curr_idx, x)
+      | _ -> idx_min in
+    List.foldi list ~f:find_min ~init:(-1,0)
+;;
 
+let min_index m = List.map m (fun row -> find_min_index_val row);;
 
+let count_mismatches al bl =
+  List.fold2_exn al bl ~init:0 ~f:(fun misses a b -> if a <> b then misses+1 else misses)
+;;
 
-(* Display one of the digits from the database *)
 let () =
-  let _, training_images = get_training_images in
- 
-  List.nth training_images 10 |> Display.display_image
-    ;;
 
+  let train_size = 5000 in
+  let test_size  = 20 in
 
+  printf "Training set size = %d\n%!" train_size;
+  printf "Test set size = %d\n%!" test_size;
 
-(* Simple test data. *)
-let test_labels = ['d';'b'];;
-let test_digits = [[0;0;0;4];[0;2;0;0]];;
-let train_labels = ['d';'b';'a';'c'];;
-let train_digits = [[0;0;0;5];[0;3;0;0];[1;0;0;0];[0;0;7;0]];;
+  printf "Loading training data..\n%!";
+  let _,train_digits = get_training_images in
+  let train_digits = List.slice train_digits 0 train_size in
+  let train_labels = List.slice get_training_labels 0 train_size in
 
-let train_size = 100;;
-let test_size  = 2;;
+  printf "Loading test data..\n%!";
+  let _,test_digits  = get_test_images in
+  let test_digits = List.slice test_digits 0 test_size in
+  let test_labels = List.slice get_test_labels 0 test_size in
 
-let _,train_digits = get_training_images;;
-let train_digits = List.slice train_digits 0 train_size;;
-let train_labels = List.slice get_training_labels 0 train_size;;
+  printf "Computing predicted test digits..\n%!";
+  (* Find the distance from all test digits to all training digits. *)
+  let d = squared_euclidean_distance test_digits train_digits in
 
-let _,test_digits  = get_test_images;;
-let test_digits = List.slice test_digits 0 test_size;;
-let test_labels = List.slice get_test_labels 0 test_size;;
+  (* For each test digit find the closest training digit. *)
+  let mins = min_index d in
 
-(* Find the distance from all test digits to all training digits. *)
-let d = squared_euclidean_distance test_digits train_digits;;
+  (* Look up the labels for the closest training digit. *)
+  let predicted_test_labels = List.map mins (fun (idx,min_dist) -> List.nth_exn train_labels idx) in
 
-(* For each test digit find the closest training digit. *)
-let mins = min_index d;;
+  let num_misses = count_mismatches test_labels predicted_test_labels in
 
-(* Look up the labels for the closest training digit. *)
-let predicted_test_labels = List.map mins (fun (m,i) -> List.nth_exn train_labels i);; 
+  let misclassification_rate = float num_misses /. float test_size in
 
-let success = test_labels = predicted_test_labels;;
-
+  printf "Misclassification rate = %f\n%!%!" misclassification_rate;
